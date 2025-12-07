d2d465dd54e86a1362e404021181e5241e356673
Leonardo-Chiarparin <leo20021972@libero.it>
2025-12-07 18:05:41 +0100

    Terms.kt, Intro.kt visually implemented; some modularization introduced

--- app/src/main/java/com/example/brave_sailors/ui/theme/Color.kt
+++ app/src/main/java/com/example/brave_sailors/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.example.brave_sailors.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// -- DEFAULT COLORS ( by Android Studio ) --
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// -- CUSTOM COLORS ( by us ) --
+val Blue = Color(color = 0xFF101529)
+val DarkBlue = Color(0xFF181D3B)
+val DeepBlue = Color(0xFF080E1C)
+val LightBlue = Color(0xFF1E2341)
+val Orange = Color(0xFFBC6826)
+val Grey = Color(0xFF343D4E)
+val DarkGrey = Color(0xFF141828)
+val LightGrey = Color(0xFF9097AA)
+val TransparentGrey = Color(0xFF434A5D)
+val White = Color(0xFFFFFFFA)
