67ca9418453cd70ae9c260d1cc1f9c25e2cecaa6
Leonardo-Chiarparin <leo20021972@libero.it>
2026-01-19 11:39:36 +0100

    Arrangement of agreed components

--- app/src/main/java/com/example/brave_sailors/Profile.kt
+++ app/src/main/java/com/example/brave_sailors/Profile.kt
@@ -3,7 +3,6 @@ package com.example.brave_sailors
 import android.Manifest
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
